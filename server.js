e302cf2bd59bc0f9bb963be0dcfd3d957facd9a2
Chris Brown <chris@thebrown.net>
2015-05-30 16:44:28 +0100

    Configurable port number

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var argv = require('optimist')
+    .usage('$0 [--debug] [--ai] [--port <port>]')
+    .default('port', 8080)
+    .argv;
+
 var express = require('express');
 var app = express();
 app.use(express.static('web'));
@@ -7,15 +12,12 @@ app.use(express.static('web'));
 var version = require('./version');
 app.get('/version.js', version);
 
-var server = app.listen(8080);
+var server = app.listen(argv.port);
 
 var io = require('socket.io')(server);
 var createGame = require('./game');
 var createNetPlayer = require('./net-player');
 var createAiPlayer = require('./ai-player');
-
-var debugging = process.argv.indexOf('--debug') >= 0;
-var ai = process.argv.indexOf('--ai') >= 0;
 
 var pending = [];
 
@@ -33,8 +35,8 @@ io.on('connection', function (socket) {
                     game = null;
                 }
             } else {
-                game = createGame(debugging);
-                if (ai) {
+                game = createGame(argv.debug);
+                if (argv.ai) {
                     createAiPlayer(game);
                 }
             }
