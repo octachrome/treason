087d2d522c9b21b8c5d3967a05c8d6aa54bb3885
Chris Brown <chris@thebrown.net>
2015-06-24 20:33:44 +0100

    Tests for when AI should challenge the opponent's attempt to block

--- test/ai-test.js
+++ test/ai-test.js
@@ -90,4 +90,76 @@ describe('AI', function () {
             });
         });
     });
+
+    describe('Given an AI attempts a steal that will win the game', function () {
+        beforeEach(function () {
+            game._test_setInfluence(0, 'captain');
+            game._test_setInfluence(1, 'contessa');
+            game._test_setCash(0, 6);
+            game._test_setCash(1, 6);
+
+            game._test_setTurnState({
+                name: stateNames.ACTION_RESPONSE,
+                playerIdx: 0,
+                action: 'steal',
+                target: 1
+            });
+        });
+
+        describe('When the opponent attempts to block', function () {
+            beforeEach(function () {
+                testPlayer.command({
+                    command: 'block',
+                    blockingRole: 'ambassador'
+                });
+
+                return testPlayer.getNextState().then(function (state) {
+                    expect(state.state.name).to.be(stateNames.BLOCK_RESPONSE);
+                });
+            });
+
+            it('Then the computer should challenge, and win', function () {
+                return testPlayer.getNextState().then(function (state) {
+                    expect(state.state.name).to.be(stateNames.GAME_WON);
+                    expect(state.state.playerIdx).to.be(0);
+                });
+            });
+        });
+    });
+
+    describe('Given an AI attempts a steal, and the endgame is some way off', function () {
+        beforeEach(function () {
+            game._test_setInfluence(0, 'captain');
+            game._test_setInfluence(1, 'contessa');
+            game._test_setCash(0, 2);
+            game._test_setCash(1, 2);
+
+            game._test_setTurnState({
+                name: stateNames.ACTION_RESPONSE,
+                playerIdx: 0,
+                action: 'steal',
+                target: 1
+            });
+        });
+
+        describe('When the opponent attempts to block', function () {
+            beforeEach(function () {
+                testPlayer.command({
+                    command: 'block',
+                    blockingRole: 'ambassador'
+                });
+
+                return testPlayer.getNextState().then(function (state) {
+                    expect(state.state.name).to.be(stateNames.BLOCK_RESPONSE);
+                });
+            });
+
+            it('Then the computer should allow the block', function () {
+                return testPlayer.getNextState().then(function (state) {
+                    expect(state.state.name).to.be(stateNames.START_OF_TURN);
+                    expect(state.state.playerIdx).to.be(1);
+                });
+            });
+        });
+    });
 });
