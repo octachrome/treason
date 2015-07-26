6e14ec912477a121667fb5f5f6dd453501870b49
JackieNiebling <jackie@niebsoft.dk>
2015-07-26 10:28:35 +0100

    Saving Chris from being That Guy. Avoids double parsing.

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ fs.readFile('adjectives.txt', function(err, data) {
     if (err) {
         throw err;
     }
-    adjectives = data.toString().replace(/\r\n/g,'\n').split('\n');
+    adjectives = data.toString().split(/\r?\n/);
 });
 
 function randomGameName(playerName) {
