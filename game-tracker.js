ce27626fc0cdb06de0c0cfd097abffa74027b40f
Chris Brown <chris@thebrown.net>
2016-07-11 20:57:45 +0100

    Routine to unpack packed game events

--- game-tracker.js
+++ game-tracker.js
@@ -81,7 +81,7 @@ GameTracker.prototype.pack = function () {
         switch (event.type) {
             case GameTracker.TYPE_START_OF_TURN:
             case GameTracker.TYPE_GAME_OVER:
-                bytes[offset++] = (event.type << 4) | (event.whoseTurn & 7);
+                bytes[offset++] = (event.type << 4) | (event.whoseTurn & 0xf);
                 event.playerStates.forEach(function (playerState) {
                     bytes[offset++] = playerState.cash & 255;
                     bytes[offset++] = (tracker.encodeInfluence(playerState.influence[0]) << 4) |
@@ -94,15 +94,15 @@ GameTracker.prototype.pack = function () {
                 break;
             case GameTracker.TYPE_CHALLENGE_SUCCESS:
             case GameTracker.TYPE_CHALLENGE_FAIL:
-                bytes[offset++] = (event.type << 4) | (event.challenger & 7);
-                bytes[offset++] = (event.challenged & 7);
+                bytes[offset++] = (event.type << 4) | (event.challenger & 0xf);
+                bytes[offset++] = (event.challenged & 0xf);
                 break;
             case GameTracker.TYPE_BLOCK:
-                bytes[offset++] = (event.type << 4) | (event.blockingPlayer & 7);
+                bytes[offset++] = (event.type << 4) | (event.blockingPlayer & 0xf);
                 bytes[offset++] = tracker.encodeRole(event.blockingRole);
                 break;
             case GameTracker.TYPE_PLAYER_LEFT:
-                bytes[offset++] = (event.type << 4) | (event.player & 7);
+                bytes[offset++] = (event.type << 4) | (event.player & 0xf);
                 break;
         }
     });
@@ -145,22 +145,135 @@ GameTracker.prototype.encodeAction = function (action, target) {
         return (8+1) << 4;
     }
     else if (action === 'steal') {
-        return (2 << 4) | (target & 7);
+        return (2 << 4) | (target & 0xf);
     }
     else if (action === 'assassinate') {
-        return (3 << 4) | (target & 7);
+        return (3 << 4) | (target & 0xf);
     }
     else if (action === 'exchange') {
         return 4 << 4;
     }
     else if (action === 'interrogate') {
         // Secondary inquisitor action.
-        return ((8+4) << 4) | (target & 7);
+        return ((8+4) << 4) | (target & 0xf);
     }
     else if (action === 'coup') {
-        return (5 << 4) | (target & 7);
+        return (5 << 4) | (target & 0xf);
     }
     else if (action === 'income') {
         return (8+5) << 4;
     }
+};
+
+GameTracker.prototype.unpack = function (buffer, gameInfo) {
+    this.gameInfo = gameInfo;
+    var events = [];
+    var offset = 0;
+    while (offset < buffer.length) {
+        var first = buffer[offset++];
+        var type = first >> 4;
+        var event = {
+            type: type
+        };
+        events.push(event);
+        switch (type) {
+            case GameTracker.TYPE_START_OF_TURN:
+            case GameTracker.TYPE_GAME_OVER:
+                event.whoseTurn = first & 0xf;
+                event.playerStates = [];
+                for (var p = 0; p < gameInfo.playerCount; p++) {
+                    var cash = buffer[offset++];
+                    var influence = buffer[offset++];
+                    event.playerStates.push({
+                        cash: cash,
+                        influence: [this.decodeInfluence(influence >> 4), this.decodeInfluence(influence & 0xf)]
+                    });
+                }
+                if (offset < buffer.length) {
+                    events.push(this.decodeActionEvent(buffer[offset++]));
+                }
+                break;
+            case GameTracker.TYPE_CHALLENGE_SUCCESS:
+            case GameTracker.TYPE_CHALLENGE_FAIL:
+                event.challenger = first & 0xf;
+                event.challenged = buffer[offset++] & 0xf;
+                break;
+            case GameTracker.TYPE_BLOCK:
+                event.blockingPlayer = first & 0xf;
+                event.blockingRole = this.decodeRole(buffer[offset++]);
+                break;
+            case GameTracker.TYPE_PLAYER_LEFT:
+                event.player = first & 0xf;
+                break;
+        }
+    }
+    return events;
+};
+
+GameTracker.prototype.decodeInfluence = function (influenceCode) {
+    return {
+        revealed: !!(influenceCode & 8),
+        role: this.decodeRole(influenceCode & 0x7)
+    };
+};
+
+GameTracker.prototype.decodeRole = function (roleCode) {
+    if (roleCode === 1) {
+        return 'duke';
+    }
+    else if (roleCode === 2) {
+        return 'captain';
+    }
+    else if (roleCode === 3) {
+        return 'assassin';
+    }
+    else if (roleCode === 4) {
+        return this.gameInfo.roles.indexOf('ambassador') >= 0 ? 'ambassador' : 'inquisitor';
+    }
+    else if (roleCode === 5) {
+        return 'contessa';
+    }
+};
+
+GameTracker.prototype.decodeActionEvent = function (actionCode) {
+    var target;
+    var action;
+    switch (actionCode >> 4) {
+        case 1:
+            action = 'tax';
+            break;
+        case 8+1:
+            action = 'foreign-aid';
+            break;
+        case 2:
+            action = 'steal';
+            target = actionCode & 0xf;
+            break;
+        case 3:
+            action = 'assassinate';
+            target = actionCode & 0xf;
+            break;
+        case 4:
+            action = 'exchange';
+            break;
+        case 8+4:
+            action = 'interrogate';
+            target = actionCode & 0xf;
+            break;
+        case 5:
+            action = 'coup';
+            target = actionCode & 0xf;
+            break;
+        case 8+5:
+            action = 'income';
+            break;
+    }
+    var event = {
+        type: GameTracker.TYPE_ACTION,
+        action: action
+    };
+    if (target != null) {
+        event.target = target;
+    }
+    return event;
 };
